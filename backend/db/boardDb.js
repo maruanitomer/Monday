3558c7c46e05df9872d99a5b6208c6c58bff088c
maruanitomer <maruanitomer9@gmail.com>
2021-05-20 14:02:24 +0300

    first-commit starting project 05/20/2021 14:02

---
+++ backend/db/boardDb.js
@@ -0,0 +1,289 @@
+var boardsDB = [
+    {
+        "_id": "b101",
+        "title": "Robot dev proj",
+        "createdAt": 1589983468418,
+        "createdBy": {
+            "_id": "VqXsZ",
+            "fullname": "Eyal Alfasi",
+            "imgUrl": "http://some-img"
+        },
+        "members": [
+            {
+                "_id": "bUoZ6",
+                "fullname": "Eyal Alfasi",
+                "imgUrl": "https://www.google.com"
+            },
+            {
+                "_id": "povB7",
+                "fullname": "Omer levi",
+                "imgUrl": "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bWFufGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+            },
+            {
+                "_id": "kiO90",
+                "fullname": "Rami Malkmo",
+                "imgUrl": "https://i.guim.co.uk/img/media/d1a8023b09fb6f46fafed7566e742e4eb5fc0c43/0_1889_3648_2189/master/3648.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=26889f0ebdcb11daa87d9d67e435f627"
+            },
+            {
+                "_id": "bUoZ6",
+                "fullname": "puki pu",
+                "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsclWjN-FcS0bUbhGzDInEMbUiJEAaLqWWmg&usqp=CAU"
+            }
+        ],  
+        "groups": [
+            {
+                "id": "g101",
+                "title": "Group 1",
+                "cards": [
+                    {
+                        "id": "c102",
+                        "title": "Help me",
+                        "comments": [
+                            {
+                                "id": "ZdPnm",
+                                "txt": "a comment",
+                                "createdAt": 1590999817436.0,
+                                "byMember": {
+                                    "_id": "u101",
+                                    "fullname": "Roger Federer",
+                                    "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                                }
+                            }
+                        ],
+                        "members": [
+                            {
+                                "_id": "u101",
+                                "fullname": "Tal Tarablus",
+                                "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                            }
+                        ],
+                        "status":
+                        {
+                            "text": "Stuck",
+                            "color": "#61bd8f"
+                        },
+                        "priority":
+                        {
+                            "text": "Low",
+                            "color": "#61bd4f"
+                        },
+                        "createdAt": 1590999730348,
+                        "dueDate": {
+                            "start": '4/20/20',
+                            "end": '4/25/20'
+                        },
+                        "createdBy": {
+                            "_id": "u101",
+                            "fullname": "Tal Tarablus",
+                            "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                        },
+                    }
+                ],
+                "style": {"color":"ffff"}
+            }
+        ],
+        "activities": [
+            {
+                "id": "a101",
+                "txt": "Changed Color",
+                "createdAt": 154514,
+                "byMember": {
+                    "_id": "u101",
+                    "fullname": "Abi Abambi",
+                    "imgUrl": "http://some-img"
+                },
+                "card": {
+                    "id": "c101",
+                    "title": "Replace Logo"
+                }
+            }
+        ]
+    },
+    {
+        "_id": "b102",
+        "title": "Proj",
+        "createdAt": 1589983468418,
+        "createdBy": {
+            "_id": "u101",
+            "fullname": "Eran lev",
+            "imgUrl": "http://some-img"
+        },
+        "members": [
+            {
+                "_id": "bUoZ6",
+                "fullname": "Tal Tarablus",
+                "imgUrl": ""
+            },
+            {
+                "_id": "u102",
+                "fullname": "Tom cubo",
+                "imgUrl": ""
+            },
+            {
+                "_id": "u103",
+                "fullname": "Sahaf Ifrah",
+                "imgUrl": ""
+            },
+            {
+                "_id": "bUoZ6",
+                "fullname": "puki pu",
+                "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsclWjN-FcS0bUbhGzDInEMbUiJEAaLqWWmg&usqp=CAU"
+            }
+        ],
+        "groups": [
+            {
+                "id": "g102",
+                "title": "Group 2",
+                "cards": [
+                    {
+                        "id": "c104",
+                        "title": "Help me",
+                        "comments": [
+                            {
+                                "id": "ZdPnm",
+                                "txt": "a comment",
+                                "createdAt": 1590999817436.0,
+                                "byMember": {
+                                    "_id": "u101",
+                                    "fullname": "Tal Tarablus",
+                                    "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                                }
+                            }
+                        ],
+                        "members": [
+                            {
+                                "_id": "u101",
+                                "fullname": "Tal Tarablus",
+                                "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                            }
+                        ],
+                        "status":
+                        {
+                            "text": "done",
+                            "color": "#61BD4F"
+                        },
+                        "priority":
+                        {
+                            "text": "done",
+                            "color": "#61BD4F"
+                        },
+                        "createdAt": 1590999730348,
+                        "dueDate": {
+                            "start": '',
+                            "end": ''
+                        },
+                        "createdBy": {
+                            "_id": "u101",
+                            "fullname": "Tal Tarablus",
+                            "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                        },
+                    }
+                ],
+                "style": {"color":"ffff"}
+            }
+        ],
+        "activities": [
+            {
+                "id": "a101",
+                "txt": "Changed Color",
+                "createdAt": 154514,
+                "byMember": {
+                    "_id": "u101",
+                    "fullname": "Abi Abambi",
+                    "imgUrl": "http://some-img"
+                },
+                "card": {
+                    "id": "c101",
+                    "title": "Replace Logo"
+                }
+            }
+        ]
+    },
+    {
+        "_id": "b103",
+        "title": "Sprint 1",
+        "createdAt": 1589983468418,
+        "createdBy": {
+            "_id": "SWvB9",
+            "fullname": "Or Tabin",
+            "imgUrl": "http://some-img"
+        },
+        // "style": {},
+        "members": [
+            {
+                "_id": "SWvB9",
+                "fullname": "Or Tabin",
+                "imgUrl": "https://www.google.com"
+            },
+
+        ],
+        "groups": [
+            {
+                "id": "g103",
+                "title": "Group 2",
+                "cards": [
+                    {
+                        "id": "c104",
+                        "title": "Help me",
+                        "comments": [
+                            {
+                                "id": "ZdPnm",
+                                "txt": "a comment",
+                                "createdAt": 1590999817436.0,
+                                "byMember": {
+                                    "_id": "u101",
+                                    "fullname": "Tal Tarablus",
+                                    "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                                }
+                            }
+                        ],
+                        "members": [
+                            {
+                                "_id": "u101",
+                                "fullname": "Tal Tarablus",
+                                "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                            }
+                        ],
+                        "status":
+                        {
+                            "text": "done",
+                            "color": "#61BD4F"
+                        },
+                        "priority":
+                        {
+                            "text": "done",
+                            "color": "#61BD4F"
+                        },
+                        "createdAt": 1590999730348,
+                        "dueDate": {
+                            "start": '',
+                            "end": ''
+                        },
+                        "createdBy": {
+                            "_id": "u101",
+                            "fullname": "Tal Tarablus",
+                            "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                        },
+                    }
+                ],
+                "style": {"color":"ffff"}
+            }
+        ],
+        "activities": [
+            {
+                "id": "a101",
+                "txt": "Changed Color",
+                "createdAt": 154514,
+                "byMember": {
+                    "_id": "u101",
+                    "fullname": "Abi Abambi",
+                    "imgUrl": "http://some-img"
+                },
+                "card": {
+                    "id": "c101",
+                    "title": "Replace Logo"
+                }
+            }
+        ]
+    }
+]
---
+++ backend/db/userDb.js
@@ -0,0 +1,9 @@
+
+var usersDB = [
+    {
+        "_id": "u101",
+        "fullname": "admin",
+        "username": "admin",
+        "password": "admin",
+    }
+]
