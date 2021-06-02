f5c20047996ecb6caae2062d376c8b2d4d8b5fcb
maruanitomer <maruanitomer9@gmail.com>
2021-06-02 19:17:03 +0300

    basic layout for the task :)

--- frontend/src/modules/task/cmps/TaskList.jsx
+++ frontend/src/modules/task/cmps/TaskList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ChatBubbleOutlineRoundedIcon from "@material-ui/icons/ChatBubbleOutlineRounded";
 import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
-// import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 export const TaskList = ({ tasks }) => {
   return (
@@ -9,7 +9,9 @@ export const TaskList = ({ tasks }) => {
       {tasks.map((task) => {
         return (
           <div className="task-container">
-            <ExpandMoreRoundedIcon className="task-option-btn"></ExpandMoreRoundedIcon>
+            <div className="task-option-btn-container">
+              <ExpandMoreRoundedIcon className="task-option-btn"></ExpandMoreRoundedIcon>
+            </div>
             <div className="flex justify-space-between">
               {task.members.map((member) => {
                 return (
@@ -18,8 +20,27 @@ export const TaskList = ({ tasks }) => {
                   </span>
                 );
               })}
-              <ChatBubbleOutlineRoundedIcon></ChatBubbleOutlineRoundedIcon>
+              <div className="flex align-center justify-center">
+                <ChatBubbleOutlineRoundedIcon></ChatBubbleOutlineRoundedIcon>
+              </div>
             </div>
+            <button className="members flex justify-center aling-center">
+              <AccountCircleIcon></AccountCircleIcon>
+            </button>
+            <button className="status flex justify-center aling-center">
+              <span>status</span>
+            </button>
+            <button className="date flex justify-center align-center">
+              {/* <input type="date" /> */}
+              <span>23 May</span>
+            </button>
+          <div className="end flex justify-space-between">
+              <div className="start-end">
+
+              </div>
+              <div className="block-end">
+              </div>
+          </div>
           </div>
         );
       })}
