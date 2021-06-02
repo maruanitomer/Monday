d5c64b4380026b367933608d1b2f8b69f8d9696b
Dudi Samarel <dudisamarel@gmail.com>
2021-06-02 17:50:57 +0300

    he

--- frontend/src/modules/board/pages/Board.jsx
+++ frontend/src/modules/board/pages/Board.jsx
@@ -1,70 +1,84 @@
-import React, { useEffect, useState, } from 'react'
-import { addBoard } from '../../../store/actions/boardActions'
-import { useDispatch, useSelector } from 'react-redux';
-import { BoardPreview } from '../cmps/BoardPreview';
-import { BoardSideBar } from '../cmps/BoardSideBar';
-import { PopUpModal } from '../../../shared/cmps/PopUpModal'
-import { BoardAdd } from '../cmps/BoardAdd';
-import { makeStyles } from '@material-ui/core';
-import { getBoard, onSetBoards } from '../hooks/setBoards';
-import { BoardHeader } from '../cmps/BoardHeader'
+import React, { useEffect, useState } from "react";
+import { addBoard } from "../../../store/actions/boardActions";
+import { useDispatch, useSelector } from "react-redux";
+import { BoardPreview } from "../cmps/BoardPreview";
+import { BoardSideBar } from "../cmps/BoardSideBar";
+import { PopUpModal } from "../../../shared/cmps/PopUpModal";
+import { BoardAdd } from "../cmps/BoardAdd";
+import { makeStyles } from "@material-ui/core";
+import { getBoard, onSetBoards } from "../hooks/setBoards";
+import { BoardHeader } from "../cmps/BoardHeader";
 
 export const Board = ({ match }) => {
-    const [board, setBoard] = useState(null)
-    const [modal, setModal] = useState(false)
-    const { boards } = useSelector(state => state.boardModule)
-    const dispatch = useDispatch()
+  const [board, setBoard] = useState(null);
+  const [modal, setModal] = useState(false);
+  const { boards } = useSelector((state) => state.boardModule);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    onSetBoards(dispatch);
+  }, [dispatch]);
 
-    useEffect(() => {
-        onSetBoards(dispatch)
-    }, [dispatch])
+  useEffect(() => {
+    (async () => {
+      try {
+        setBoard(await getBoard(match.params.boardId, boards));
+      } catch (err) {
+        throw err;
+      }
+    })();
+  }, [boards, match.params]);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                setBoard(await getBoard(match.params.boardId, boards))
-            }
-            catch (err) { throw err }
-        })()
-    }, [boards, match.params])
+  const toggleModal = (ev) => {
+    ev.stopPropagation();
+    setModal(!modal);
+  };
 
-    const toggleModal = (ev) => {
-        ev.stopPropagation()
-        setModal(!modal);
-    }
+  //Modal Generic CSS
+  const useStyles = makeStyles({
+    popup: {
+      backgroundColor: "white",
+      position: "relative",
+      top: "25%",
+      left: "25%",
+      width: "35vw",
+      height: "35vh",
+    },
+  });
+  const classes = useStyles();
 
-    //Modal Generic CSS
-    const useStyles = makeStyles({
-        popup: {
-            backgroundColor: 'white',
-            position: 'relative',
-            top: '25%',
-            left: '25%',
-            width: '35vw',
-            height: '35vh'
-
-        }
-    })
-    const classes = useStyles();
-
-    //Adding new Board
-    const onAddBoard = async (board, ev) => {
-        ev.stopPropagation();
-        const action = await addBoard(board);
-        dispatch(action)
-        toggleModal(ev)
-    }
-    return (
-        (boards) ?
-            <div className="board-layout flex">
-                {modal && <PopUpModal toggleModal={toggleModal} popup={classes.popup}><BoardAdd onAdd={onAddBoard} toggleModal={toggleModal} /></PopUpModal>}
-                <BoardSideBar toggleModal={toggleModal} boards={boards}></BoardSideBar>
-                <div className="board-container flex column">
-                    <BoardHeader board={board}></BoardHeader>
-                    {(board) ? <BoardPreview board={board} /> : <p>Select board</p>}
-                </div>
-            </div> :
-            <h1>loading</h1>
-    )
-}
+  //Adding new Board
+  const onAddBoard = async (board, ev) => {
+    ev.stopPropagation();
+    const action = addBoard(board);
+    dispatch(action);
+    toggleModal(ev);
+  };
+  return boards ? (
+    <div className="board-layout flex">
+      {modal && (
+        <PopUpModal toggleModal={toggleModal} popup={classes.popup}>
+          <BoardAdd
+            types={[
+              "Employees",
+              "Campaigns",
+              "Projects",
+              "Creatives",
+              "Clients",
+              "Tasks",
+            ]}
+            onAdd={onAddBoard}
+            toggleModal={toggleModal}
+          />
+        </PopUpModal>
+      )}
+      <BoardSideBar toggleModal={toggleModal} boards={boards}></BoardSideBar>
+      <div className="board-container flex column">
+        <BoardHeader board={board}></BoardHeader>
+        {board ? <BoardPreview board={board} /> : <p>Select board</p>}
+      </div>
+    </div>
+  ) : (
+    <h1>loading</h1>
+  );
+};
