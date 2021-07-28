ca994a1a93c3b9ee5dc479d88a9ac6c1aada27be
Dudi Samarel <dudisamarel@gmail.com>
2021-07-28 17:25:04 +0300

    popups

--- frontend/src/modules/group/cmps/GroupPreview.jsx
+++ frontend/src/modules/group/cmps/GroupPreview.jsx
@@ -1,8 +1,38 @@
 import { TaskList } from "../../task";
+import { usePopper } from "react-popper";
+import { useState } from "react";
+import { Portal } from "../../../shared/hooks/Portal";
+import { useClickOutside } from "../../../shared/hooks/clickOutSide";
+import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
 
 export const GroupPreview = ({ group, board }) => {
+ 
+  const [refEl, setRefEl] = useState();
+  const [popperEl, setpopperEl] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  let domNode = useClickOutside(() => {
+    setIsOpen(false);
+  });
+  let { styles, attributes } = usePopper(refEl, popperEl);
   return (
-    <div >
+    <div>
+      <div ref={domNode}>
+        <button ref={setRefEl} onClick={() => setIsOpen(!isOpen)}>
+        <ExpandMoreRoundedIcon/>
+        </button>
+      </div>
+      <Portal>
+        <div
+          ref={setpopperEl}
+          hidden={!isOpen}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <li style={{backgroundColor: 'red'}}>Delete</li>
+          <li style={{backgroundColor: 'red'}}>Rename</li>
+          
+        </div>
+      </Portal>
       <div className="grid-tasks-layout">
         <span className="title">{group.title}</span>
         <span className="person">Person</span>
